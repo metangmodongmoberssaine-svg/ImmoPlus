24d704e8b9a85e6d2868d93c6ddc3243c200544e
metangmodongmoberssaine-svg <metangmodongmoberssaine@gmail.com>
2026-09-14 12:19:17 +0100

    feat:creation du model Owner et migration de la table owners

---
+++ database/migrations/2026_09_14_111017_create_owners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            //si un propritaire est lié é un compte utilisateur (User)
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique;
+            $table->string('phone');
+            $table->string('address')->nullable();
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('owners');
+    }
+};
